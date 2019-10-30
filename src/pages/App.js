2f1f5907a21a88b322149220343025082d633a58
browniu <niuniusoga@qq.com>
2019-10-30 18:06:41 +0800

    初始化

---
+++ src/pages/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function () {
+    return(
+        <div className={'app'}>
+            <h1>Bronwiu</h1>
+            <p>这个是我自己配置的工程模版</p>
+        </div>
+    )
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: "development",
+    entry: ['./src/index.js'],
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        watchContentBase: true,
+        compress: false,
+        hot: true,
+        // hotOnly: true,
+        quiet: true,
+        clientLogLevel: 'none'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|mjs|ts|tsx)$/,
+                // include: 'src',
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                loader: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
